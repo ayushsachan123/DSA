b5d63f844115771d59b677f680b2c1b148969cd7
Ayush Sachan <97084090+ayushsachan123@users.noreply.github.com>
2022-11-21 22:39:40 +0530

    Create Taylor_Series_Horners_Rule.cpp

---
+++ Abdul_Bari/Recursion/Taylor_Series_Horners_Rule.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+double e(int x,int n){  
+static double s=1;
+if(n==0)
+return s;
+s=1+x*s/n;
+return e(x,n-1);
+}
+int main(){
+    double r=e(1,10);
+cout<<r;
+    return 0;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Output--
+2.71828
