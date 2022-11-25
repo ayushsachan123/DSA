9b5e973267aea0c95c52b01e5915ee82bab93152
Ayush Sachan <97084090+ayushsachan123@users.noreply.github.com>
2022-11-25 22:33:57 +0530

    Create Static_Variable_In_Recursion.cpp

---
+++ Abdul_Bari/Recursion/Static_Variable_In_Recursion.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+  
+
+int fun(int n){
+    static int x=0;
+    if(n>0){
+        x++;
+        return fun(n-1)+x;
+    }
+ return 0;
+}
+int main(){
+    int a=5;
+    int r=fun(a);
+    cout<<r<<endl;
+    return 0;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Output--
+// 25
